7aef043a15799b167be0ad392bb5d56cbe93f99f
sladeshka <130031mail@mail.ru>
2023-11-21 14:11:13 +0300

    additional tasks A.1

---
+++ task_A_1/A.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A._1
+{
+    internal class Program
+    {
+        private static double Sqrt(double value)
+        {
+            double x = 1;
+            double oldx;
+            do
+            {
+                oldx = x;
+                x = (x + value / x) / 2;
+            }
+            while (oldx != x);
+            return x;
+        }
+        public static void Main()
+        {
+
+            double target = 2023;
+            double x = Sqrt(target);
+            Console.WriteLine(x);
+            Console.WriteLine(x * x);
+        }
+    }
+}
