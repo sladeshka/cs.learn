ef80765e6d3d5b438179ad0b684c4089ca4bfc74
sladeshka <130031mail@mail.ru>
2023-11-23 22:14:39 +0300

    lab 4.3 (task_4_3)

---
+++ task_4_3/Utils/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            int f;
+            Console.WriteLine("Enter x:");
+            int x = int.Parse(Console.ReadLine());
+            bool ok = Utils.Factorial(x, out f);
+            if(ok)
+            {
+                Console.WriteLine($"Factorial({x}) = {f}");
+            } else {
+                Console.WriteLine("Cannot compute this factorial");
+            }
+        }
+    }
+}
---
+++ task_4_3/Utils/Utils.cs
@@ -0,0 +1,30 @@
+﻿namespace Utils
+{
+    internal class Utils
+    {
+        public static bool Factorial(int n, out int answer)
+        {
+            int f = 1;
+            bool ok = true;
+            {
+                try
+                {
+                    checked {
+                        for (int k = 2; k <= n; ++k)
+                        {
+                            f *= k;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    f = 0;
+                    ok = false;
+                }
+
+            }
+            answer = f;
+            return ok;
+        }
+    }
+}
