64f3503746d850363ca4e021de13287f0e6232c9
sladeshka <130031mail@mail.ru>
2023-11-23 21:27:00 +0300

    lab 4.1 (task_4_1)

---
+++ task_4_1/Utils/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x:");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter y:");
+            int y = int.Parse(Console.ReadLine());
+            int greater = Utils.Greater(x, y);
+            Console.WriteLine($"The greater of the two {greater}");
+        }
+    }
+}
---
+++ task_4_1/Utils/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    internal class Utils
+    {
+        public static int Greater(int x, int y)
+        {
+            if (x > y)
+                return x;
+            else
+                return y;
+        }
+    }
+}
