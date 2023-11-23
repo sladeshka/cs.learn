4fb1089acc22cca240a37f8e3f57843bfb6b8c3e
sladeshka <130031mail@mail.ru>
2023-11-23 21:43:26 +0300

    lab 4.2 (task_4_2)

---
+++ task_4_2/Utils/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    internal class Test
+    {
+        public static void Main()
+        {
+            int x = 12;
+            int y = 21;
+            Console.WriteLine($"before swap x = {x} and y = {y}");
+            Utils.Swap(ref x, ref y);
+            Console.WriteLine($"after swap x = {x} and y = {y}");
+        }
+    }
+}
---
+++ task_4_2/Utils/Utils.cs
@@ -0,0 +1,12 @@
+﻿namespace Utils
+{
+    public class Utils
+    {
+        public static void Swap(ref int x, ref int y)
+        {
+            int z = x;
+            x = y;
+            y = z;
+        }
+    }
+}
