aa562f9f40bb880b29309dce87f3386d23c752f3
sladeshka <130031mail@mail.ru>
2023-11-26 17:55:52 +0300

    lab 5.1 (task_5_1)

---
+++ task_5_1/FileDetails/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualBasic;
+
+namespace FileDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            // string fileName = args[0];
+            string fileName = "text.txt";
+            FileStream stream = new FileStream(fileName, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+            int size = (int)stream.Length;
+            char[] contents = new char[size];
+            for (int i = 0; i < size; i++)
+            {
+                contents[i] = (char)reader.Read();
+            }
+            reader.Close();
+            FileDetails.Summarize(contents);
+            //foreach (char ch in contents)
+            //{
+            //    Console.Write(ch);
+            //}
+        }
+    }
+
+    internal class FileDetails
+    {
+        public static void Summarize (char[] contents) {
+            int vowels = 0, consonants = 0, lines = 0; 
+            foreach (char current in contents)
+            {
+                if (Char.IsLetter(current))
+                {
+                    if ("AEIOUaeiou".IndexOf(current) != -1)
+                    {
+                        vowels++;
+                    }
+                    else { consonants++; }
+                }
+                else if (current == '\n') { lines++; }
+            }
+            Console.WriteLine("Total no of characters: {0}", contents.Length);
+            Console.WriteLine("Total no of vowels : {0}", vowels);
+            Console.WriteLine("Total no of consonants: {0}", consonants);
+            Console.WriteLine("Total no of lines : {0}", lines);
+        }
+    }
+}
