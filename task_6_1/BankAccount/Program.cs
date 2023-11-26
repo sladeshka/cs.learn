65a0268166e0d3c33eff8cf7ef7be0ae7a61a168
sladeshka <130031mail@mail.ru>
2023-11-26 22:48:31 +0300

    lab 6.1 (task_6_1)

---
+++ task_6_1/BankAccount/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace BankAccount
+{
+    class CreateAccount
+    {
+        static void Main()
+        {
+            BankAccount berts = NewBankAccount();
+            Write(berts);
+
+            BankAccount freds = NewBankAccount();
+            Write(freds);
+        }
+
+        static BankAccount NewBankAccount()
+        {
+            BankAccount created = new BankAccount();
+
+            Console.Write("Enter the account number   : ");
+            long number = long.Parse(Console.ReadLine());
+
+            Console.Write("Enter the account balance! : ");
+            decimal balance = decimal.Parse(Console.ReadLine());
+
+            created.Populate(number, balance);
+
+            return created;
+        }
+
+        static void Write(BankAccount bankAccount)
+        {
+            Console.WriteLine($"Account number is {bankAccount.getNumber()}");
+            Console.WriteLine($"Account balance is {bankAccount.getBalance()}");
+            Console.WriteLine($"Account type is {bankAccount.getType()}");
+        }
+    }
+
+    class BankAccount
+    {
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+        public void Populate(long number, decimal balance)
+        {
+            accNo = number;
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+
+        public long getNumber()
+        {
+            return accNo;
+        }
+
+        public decimal getBalance()
+        {
+            return accBal;
+        }
+
+        public string getType()
+        {
+            return accType.ToString();
+        }
+    }
+
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+}
