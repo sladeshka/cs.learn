af8aa9812d8ba435e4b20f7474718c66ef170692
sladeshka <130031mail@mail.ru>
2023-11-28 20:10:17 +0300

    lab 7.2 (task_7_2)

---
+++ task_7_2/Utils/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Utils
+{
+    internal class Utils
+    {
+        public static void Revers(ref string s)
+        {
+            string SRev = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                SRev += s[i];
+            }
+            s = SRev;
+        }
+    }
+}
---
+++ task_7_2/Utils/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    internal class Test
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter string to reverse:");
+            string message = Console.ReadLine();
+            Utils.Revers(ref message);
+            Console.WriteLine(message);
+        }
+    }
+}
