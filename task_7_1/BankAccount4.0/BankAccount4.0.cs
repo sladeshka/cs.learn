b68bf4022ab57ce4dad7f5e3a3d3704e2cb5f6cf
sladeshka <130031mail@mail.ru>
2023-11-28 19:53:09 +0300

    lab 7.1 (task_7_1)

---
+++ task_7_1/BankAccount4.0/BankAccount4.0.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace BankAccount4._0
+{
+    class BankAccount
+    {
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+        private static long nextAccNo = 123;
+        public void Populate(decimal balance)
+        {
+            accNo = NextNumber();
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+
+        public long getNumber()
+        {
+            return accNo;
+        }
+
+        public decimal getBalance()
+        {
+            return accBal;
+        }
+
+        public string getType()
+        {
+            return accType.ToString();
+        }
+
+        private static long NextNumber()
+        {
+            return nextAccNo++;
+        }
+
+        public decimal? Deposit(decimal amount)
+        {
+            try
+            {
+                if (amount >= 0)
+                {
+                    accBal += amount;
+                    return accBal;
+                }
+                else
+                {
+                    throw new Exception("The deposit cannot be negative");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            try
+            {
+                bool sufficientFunds = checkFunds(amount);
+                if (sufficientFunds)
+                {
+                    accBal -= amount;
+                    return sufficientFunds;
+                }
+                else
+                {
+                    throw new Exception("Insufficient funds.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+        }
+
+        private bool checkFunds(decimal amount)
+        {
+            return accBal >= amount;
+        }
+
+        public void TransferFrom(BankAccount bankAccount, decimal amount)
+        {
+            if (bankAccount.Withdraw(amount))
+            {
+                this.Deposit(amount);
+            }
+        }
+    }
+
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+}
---
+++ task_7_1/BankAccount4.0/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount4._0
+{
+    internal class Test
+    {
+        static void Main()
+        {
+            BankAccount bankAccount1 = new BankAccount();
+            bankAccount1.Populate(100);
+            BankAccount bankAccount2 = new BankAccount();
+            bankAccount2.Populate(100);
+            Console.WriteLine("Before transfer");
+            Console.WriteLine($"{bankAccount1.getType()} {bankAccount1.getNumber()} {bankAccount1.getBalance()}");
+            Console.WriteLine($"{bankAccount2.getType()} {bankAccount2.getNumber()} {bankAccount2.getBalance()}");
+            bankAccount1.TransferFrom(bankAccount2, 10);
+            Console.WriteLine("After transfer");
+            Console.WriteLine($"{bankAccount1.getType()} {bankAccount1.getNumber()} {bankAccount1.getBalance()}");
+            Console.WriteLine($"{bankAccount2.getType()} {bankAccount2.getNumber()} {bankAccount2.getBalance()}");
+        }
+    }
+}
