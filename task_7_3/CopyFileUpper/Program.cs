b45a829cdb7372d23c7d1cdbcff10916e0bc5465
sladeshka <130031mail@mail.ru>
2023-11-28 21:56:18 +0300

    lab 7.3 (task_7_3)

---
+++ task_7_3/CopyFileUpper/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.IO;
+class CopyFileUpper
+{
+    static void Main()
+    {
+        Console.Write("Copy from file:");
+        string fileFrom = Console.ReadLine();
+        Console.Write("Copy to file:");
+        string fileTo = Console.ReadLine();
+        Console.WriteLine($"Copy from file {fileFrom} to file {fileTo}");
+        try
+        {
+            StreamReader sr  = new(fileFrom);
+            StreamWriter sw = new(fileTo);
+            while (sr.Peek() != -1)
+            {
+                sw.WriteLine(sr.ReadLine().ToUpper());
+            }
+            sw.Close();
+            sr.Close();
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Input file not found");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unexpected exception");
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
