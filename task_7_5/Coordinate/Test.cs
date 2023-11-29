1fc45c5fee309f728dd4c2cda59e68a87502b4a1
sladeshka <130031mail@mail.ru>
2023-11-29 19:51:59 +0300

    lab 7.5 (task_7_5)

---
+++ task_7_5/Coordinate/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace Coordinate
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            string str = "x";
+            Coordinate coordinate = new();
+            Utils.Display(x);
+            Utils.Display(str);
+            Utils.Display(coordinate);
+        }
+    }
+}
---
+++ task_7_5/Coordinate/Utils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinate
+{
+    interface IPrintable
+    {
+        void Print();
+    }
+    internal class Utils
+    {
+        public static void Display(object obj)
+        {
+            IPrintable ip = (obj as IPrintable);
+            if (ip != null)
+            {
+                ip.Print();
+            } 
+            else
+            {
+                Console.WriteLine(obj.ToString());
+            }
+        }
+    }
+
+    class Coordinate : IPrintable
+    {
+        private double x;
+        private double y;
+
+        public Coordinate()
+        {
+            x = 0.0;
+            y = 0.0;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"({x},{y})");
+        }
+    }
+}
