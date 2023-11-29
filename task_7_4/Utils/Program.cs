f3190b7ab21c001e45294e45c7eb725876ec6609
sladeshka <130031mail@mail.ru>
2023-11-29 19:28:34 +0300

    lab 7.4 (task_7_4)

---
+++ task_7_4/Utils/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Utils
+{
+    internal class Utils
+    {
+        public static bool IsItFormattable(object obj)
+        {
+            if (obj is IFormattable) return true;
+            return false;
+        }
+    }
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            ulong y = 0;
+            string z = "x";
+            Console.WriteLine(Utils.IsItFormattable(x));
+            Console.WriteLine(Utils.IsItFormattable(y));
+            Console.WriteLine(Utils.IsItFormattable(z));
+        }
+    }
+}
