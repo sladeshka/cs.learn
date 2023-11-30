78eaae8cfcc7af930243211c24d09d77a0a00c17
sladeshka <130031mail@mail.ru>
2023-11-30 21:20:06 +0300

    lesson 10

---
+++ lesson_10/Lesson10/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Lesson10
+{
+    internal class Program
+    {
+        class Dice
+        {
+            protected int point { get => new Random().Next(1, 7); }
+        }
+
+        class Game : Dice
+        {
+            private Dice dice;
+            public Game()
+            {
+                dice = new();
+            }
+            public int rollDice()
+            {
+                return base.point;
+            }
+        }
+
+        class Gamer
+        {
+            private string name;
+            public Gamer(string name)
+            {
+                this.name = name;
+            }
+
+            public override string ToString()
+            {
+                return name;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Gamer gamer = new("First");
+            Game game = new();
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Points rolled {game.rollDice()} points for {gamer}");
+            }
+        }
+    }
+}
