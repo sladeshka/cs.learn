342c8eaf45a89068bfbf8423d597fac2e4f74493
sladeshka <130031mail@mail.ru>
2023-11-30 20:46:48 +0300

    lab 7.6 (task_7_6)

---
+++ task_7_6/Book/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Xml.Linq;
+
+namespace Book
+{
+    internal class Book : IComparable
+    {
+        private string author;
+        private string title;
+        private int pages;
+        private int year;
+        public Book(string author, string title, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.pages = pages;
+            this.year = year;
+        }
+        public int CompareTo(object? obj) {
+            Book comparedBook = (Book)obj;
+            if (this.year == comparedBook.year) return 0;
+            else if (this.year > comparedBook.year) return 1;
+            else return -1;
+        }
+
+        public override string ToString()
+        {
+            return $"\nBook:\nAuthor: {author}\nTitle:{title}\nPublished:{year}\n{pages} pages.\n";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = {
+                new("Harper Lee", "To Kill a Mockingbird", 281, 1960),
+                new("F. Scott Fitzgerald", "The Great Gatsby", 208, 1925)
+            };
+            Array.Sort(books);
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
